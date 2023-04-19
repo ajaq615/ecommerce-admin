962a466742483ee86f9e8222fb494ad95f88c503
ajquidasol615 <ajquidasol615@gmail.com>
2023-04-19 14:31:36 +0800

    ..

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,8 +7,11 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
-      <div className='bg-red-800 text-white text-lg'>test</div>
-    </>
+          <div className='bg-blue-900 w-screen h-screen flex items-center'>
+            <div className='text-center w-full'>
+              <button className='bg-white p-2 px-4 rounded-lg'>Login with Google</button>
+            </div>
+          </div>
+
   )
 }
